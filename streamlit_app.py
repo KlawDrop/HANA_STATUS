fa70bc5108698c99a91247d884700719d758b2b0
KlawDrop <arkimosss.cdl@gmail.com>
2024-08-15 02:36:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
+# Titre de l'application
 st.title("Status")
+
+# Texte avec lien
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-st.HeaderActionElements("Weeee")
 )
+
+# En-tête
+st.header("Weeee")
